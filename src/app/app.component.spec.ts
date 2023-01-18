8bfa9058e3078b6bce49a042f972e5c56956e06d
Rubens de Andrade <rubensaz14@gmail.com>
2023-01-17 21:08:24 -0300

    chore: added pre commit and pre push hooks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,8 +1,8 @@
-import { TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
-import { AppComponent } from "./app.component";
+import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
 
-describe("AppComponent", () => {
+describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule],
@@ -10,7 +10,7 @@ describe("AppComponent", () => {
     }).compileComponents();
   });
 
-  it("should create the app", () => {
+  it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
@@ -19,15 +19,15 @@ describe("AppComponent", () => {
   it(`should have as title 'base-project'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual("base-project");
+    expect(app.title).toEqual('base-project');
   });
 
-  it("should render title", () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector(".content span")?.textContent).toContain(
-      "base-project app is running!"
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'base-project app is running!'
     );
   });
 });
