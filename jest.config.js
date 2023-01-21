c20cf8c2ed58150dc0fbf0d47654e22d90933cc5
Rubens de Andrade <rubensaz14@gmail.com>
2023-01-21 17:18:29 -0300

    test: improved jest configs with code coverage

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,21 @@ module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  collectCoverageFrom: ['src/**/*.ts'],
+  coveragePathIgnorePatterns: [
+    'node_modules',
+    '.mock.ts',
+    '.module.ts',
+    '<rootDir>/src/main.ts',
+    '<rootDir>/src/environments',
+  ],
+  coverageDirectory: '<rootDir>/coverage/',
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1,
+    },
+  },
 };
