1980025d56a48e4151234efe741198d55e9619fc
Rubens de Andrade <rubensaz14@gmail.com>
2023-01-30 19:27:01 -0300

    test: added module mapper to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,12 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+const modulePaths = pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' });
+
 module.exports = {
   preset: 'jest-preset-angular',
   setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
   globalSetup: 'jest-preset-angular/global-setup',
+  moduleNameMapper: { ...modulePaths },
   collectCoverageFrom: ['src/**/*.ts'],
   coveragePathIgnorePatterns: ['node_modules', '.mock.ts', '.module.ts', '<rootDir>/src/main.ts', '<rootDir>/src/environments'],
   coverageDirectory: '<rootDir>/coverage/',
