ab28b0224bfb1712e2f5846c905e4b6f1bf956a7
Rubens de Andrade <rubensaz14@gmail.com>
2023-01-31 20:32:15 -0300

    refactor: removed default template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,19 +13,7 @@ describe('AppComponent', () => {
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have as title 'base-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('base-project');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('base-project app is running!');
+    expect(app).toBeTruthy();
   });
 });
